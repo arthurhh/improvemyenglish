a439846a67226c8c7b1957255e295e31de4c31eb
arthurhh <105168759+arthurhh@users.noreply.github.com>
2023-04-26 11:03:08 +0200

    Rewrite the code to work on the current state of the website

--- dailyReading.py
+++ dailyReading.py
@@ -1,122 +1,121 @@
-import json
+from webdriver_manager.chrome import ChromeDriverManager
+import undetected_chromedriver as uc
+from selenium.webdriver.chrome.service import Service as ChromeService
 import os
-import ssl
 from datetime import datetime
-
-import pygit2
-import requests
-import urllib3
-import urllib
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import urllib.request
 import re
-from bs4 import BeautifulSoup
-from github import Github
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as cond
+import wisecreator.wisecreate
 
 import wisecreator.wisecreate
 
-repoName = os.environ['REPO_NAME']
-repoToken = os.environ['GITHUB_TOKEN']
-repo_link = os.environ['REPO_LINK']
 domain = os.environ['HIDE_DOMAIN']
-userName = os.environ['USER_NAME']
-email = os.environ['EMAIL']
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1 Safari/605.1.15'}
-http = urllib3.PoolManager(10, headers=headers, cert_reqs=ssl.CERT_NONE)
-urllib3.disable_warnings()
+timeout = os.environ['TIMEOUT']
 
 
-def get_element_from_request(url, element, class_):
-    response = http.request('GET', url)
-    soup = BeautifulSoup(response.data.decode('utf-8'), "html5lib")
-    return soup.find(element, class_=class_)
+def get_meta_data(driver):
+    driver.get(domain)
+    WebDriverWait(driver, timeout).until(
+        cond.visibility_of_element_located((By.CLASS_NAME, "cookie-disclaimer__cta"))).click()
+    start_button = WebDriverWait(driver, timeout).until(
+        cond.visibility_of_element_located((By.CSS_SELECTOR, "[data-test-id='view-daily-blink-button']")))
+    article = driver.find_element(By.XPATH, "//article")
+    title = article.find_element(By.TAG_NAME, "h2").get_attribute('innerHTML').strip()
+    author = article.find_element(By.XPATH, "//div[3]/div/p").get_attribute('innerHTML').strip()
+    subtitle = article.find_element(By.XPATH, "//div[3]/div/div[1]").get_attribute('innerHTML').strip()
+    img_url = article.find_element(By.TAG_NAME, "img").get_attribute('src')
+    description = driver.find_element(By.CSS_SELECTOR, "[data-test-id='about-section']") \
+        .find_element(By.XPATH, "//p/p").get_attribute('innerHTML').strip()
+
+    return title, author, subtitle, description, img_url, start_button
 
 
-def get_meta_data():
-    container = get_element_from_request(domain + '/nc/daily', 'div', 'daily-book__container')
-
-    title = container.find('h3', 'daily-book__headline').string.strip()
-    author = container.find('div', 'daily-book__author').string.strip()
-    description = container.find('div', 'book-tabs__content-inner').text.strip()
-    cta = container.find('a', 'cta cta--play daily-book__cta')['href']
-    img_url = container.find('img')['src']
-
-    return title, author, description, cta, img_url
-
-
-def get_article(cta):
-    return str(get_element_from_request(domain + cta, 'article',
-                                        'shared__reader__blink reader__container__content')).strip()
-
-
-def get_chapter_ids(cta):
-    chapter_no_2_chapter_id = {}
-    menu_list = get_element_from_request(f'{domain}{cta}', "div",
-                                         "reader__container__chapters__menu__inside")
-    for li in menu_list.find_all('li'):
-        chapter_no_2_chapter_id[li.attrs.get('data-chapterno')] = li.attrs.get('data-chapterid')
-    return chapter_no_2_chapter_id
+def get_article(driver, start_button):
+    next_button = start_button
+    html_article = ""
+    audio_links = {}
+    chapter_counter = 0
+    while next_button:
+        next_button.click()
+        wait_for_loading_screen(driver)
+        wait_for_reader_page(driver)
+        next_button = get_next_button(driver)
+        audio_link = driver.find_element(By.CSS_SELECTOR, "[data-test-id='readerAudio']").get_attribute("audio-url")
+        chapter_headline = driver.find_element(By.CLASS_NAME, "reader-content__headline").get_attribute('innerHTML')
+        chapter = driver.find_element(By.CLASS_NAME, "reader-content__text").get_attribute('innerHTML')
+        html_article += f'<h1>{chapter_headline}</h1>'
+        html_article += chapter
+        audio_links[chapter_counter] = audio_link
+        chapter_counter += 1
+    return html_article, audio_links
 
 
-def get_book_id(cta):
-    reader_contrainer = get_element_from_request(f'{domain}{cta}', "div", "reader__container")
-    return reader_contrainer.attrs.get('data-book-id')
+def wait_for_loading_screen(driver):
+    WebDriverWait(driver, timeout).until(cond.visibility_of_element_located((By.CLASS_NAME, "loader")))
+    WebDriverWait(driver, timeout).until(cond.invisibility_of_element_located((By.CLASS_NAME, "loader")))
 
 
-def get_audio_links(book_id, chapterids):
-    chapter_no_2_audio_link = {}
-    for chapterNo, chapterid in chapterids.items():
-        response = requests.get(f'{domain}/api/books/{book_id}/chapters/{chapterid}/audio',
-                                headers={'x-requested-with': 'XMLHttpRequest'})
-        chapter_no_2_audio_link[chapterNo] = json.loads(response.content).get('url')
-    return chapter_no_2_audio_link
+def wait_for_reader_page(driver):
+    WebDriverWait(driver, timeout).until(cond.visibility_of_element_located((By.CLASS_NAME, "reader-content__text")))
 
 
-def write_audio_file(audio_links, directory, title):
+def get_next_button(driver):
+    button_in_list = driver.find_elements(By.CSS_SELECTOR, "[data-test-id='nextChapter']")
+    if button_in_list and button_in_list[0].is_displayed():
+        return button_in_list[0]
+    else:
+        return None
+
+
+def write_audio_file(opener, audio_links, directory, title):
     for chapterNo, audio_link in audio_links.items():
-        write_to_file(os.path.join(directory, f'{title}-{chapterNo}.m4a'),
-                      requests.get(audio_link).content, 'wb')
+        opener.retrieve(audio_link, os.path.join(directory, f'{title}-{chapterNo}.m4a'))
+
+
+def create_webdriver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+    driver = uc.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+
+    return driver
 
 
 def run():
     print('Fetching content...', end='')
-    title, author, description, cta, img_url = get_meta_data()
-    html_article = get_article(cta)
+    driver = create_webdriver()
+    title, author, subtitle, description, img_url, start_button = get_meta_data(driver)
+    html_article, audio_links = get_article(driver, start_button)
     date = datetime.now().strftime('%Y%m%d')
-    author = re.sub('[^A-Za-z0-9]+', '_', author.replace('by ', ''))
+    legal_author = re.sub('[^A-Za-z0-9]+', '_', author.replace('by ', ''))
     legal_title = re.sub('[^A-Za-z0-9]+', '_', title)
-    directory = 'clone/blinks/' + f'{date[:4]}' + '/' + legal_title
+    directory = 'blinks/' + f'{date[:4]}' + '/' + legal_title
     if not os.path.exists(directory):
         os.makedirs(directory)
+    opener = urllib.request.URLopener()
+    opener.addheader('User-Agent', driver.execute_script("return navigator.userAgent;"))
     image_name = os.path.join(directory, str(img_url).split('/')[-1])
-    urllib.request.urlretrieve(img_url, image_name)
-    output_html = f'<h1>{title}</h1><h2>{author}</h2><p>{description}</p>{html_article}'
+    opener.retrieve(img_url, image_name)
+    output_html = f'<h1>{title}</h1><h2>{author}</h2><h3>{subtitle}</h3><p>{description}</p>{html_article}'
 
-    commitMessage = f'{title} by {author}'
-    html_file_name = os.path.join(directory, f'{date}-{legal_title}-{author}.html')
+    commit_message = f'{title} by {author}'
+    html_file_name = os.path.join(directory, f'{date}-{legal_title}-{legal_author}.html')
 
     print('Building output...', end='')
     write_to_file(html_file_name, output_html, 'w')
     language_file_name = wisecreator.wisecreate.main(f'{html_file_name}', image_name)
-    write_index_file(date, legal_title, author, language_file_name)
+
     os.remove(html_file_name)
     os.remove(image_name)
-    write_audio_file(get_audio_links(get_book_id(cta), get_chapter_ids(cta)), directory, legal_title)
-    return commitMessage
+    write_audio_file(opener, audio_links, directory, legal_title)
+    return commit_message
 
-def write_index_file(date, legal_title, author, language_file_name):
-    content = f'{date}-{legal_title}-{author}'
-    print(content)
-    link_to_mobi = f'{repo_link}/raw/master/blinks/2019/{legal_title}/' \
-                   f'{content}/{content}.mobi'
-    print(link_to_mobi)
-    link_to_dll = f'{repo_link}/raw/master/blinks/2019/{legal_title}/' \
-                  f'{content}/{content}.sdr/{language_file_name}'
-    file_content = f'{content}\n{link_to_mobi}\n{link_to_dll}'
-    index_directory = 'clone/blinks/' + f'{date[:4]}' + '/index'
-    if not os.path.exists(index_directory):
-        os.makedirs(index_directory)
-    write_to_file(os.path.join(index_directory, f'{date}.txt'), file_content, 'w')
 
 def write_to_file(file_name, output, option):
     file = open(file_name, option)
@@ -124,30 +123,5 @@ def write_to_file(file_name, output, option):
     file.close()
 
 
-def get_clone_repo():
-    g = Github(repoToken)
-    repo = g.get_user().get_repo(repoName)
-    repo_clone = pygit2.clone_repository(repo.git_url, 'clone')
-    repo_clone.remotes.set_url("origin", repo.clone_url)
-    return repo_clone
-
-
-def commit_and_push(repo_clone, commit_message):
-    index = repo_clone.index
-    index.add_all()
-    index.write()
-    author = pygit2.Signature(userName, email)
-    commiter = pygit2.Signature(userName, email)
-    tree = index.write_tree()
-    repo_clone.create_commit('refs/heads/master', author, commiter, commit_message, tree,
-                             [repo_clone.head.get_object().hex])
-    remote = repo_clone.remotes["origin"]
-    credentials = pygit2.UserPass(repoToken, 'x-oauth-basic')
-    remote.credentials = credentials
-    callbacks = pygit2.RemoteCallbacks(credentials=credentials)
-    remote.push(['refs/heads/master'], callbacks=callbacks)
-
-
 if __name__ == '__main__':
-    clone = get_clone_repo()
-    commit_and_push(clone, run())
+    run()
